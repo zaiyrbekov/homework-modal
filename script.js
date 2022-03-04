cef8251a9488111d44420c9c5fad3fa1c148493b
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-03-04 12:50:40 +0600

    added img file

--- script.js
+++ script.js
@@ -4,16 +4,59 @@ var btn = document.getElementById("myBtn");
 
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
+
+
+
+var modal = document.getElementById("myModal-2");
+
+var btn = document.getElementById("myBtn-2");
+
+var span = document.getElementsByClassName("close-2")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+
+var modal = document.getElementById("myModal-3");
+
+var btn = document.getElementById("myBtn-3");
+
+var span = document.getElementsByClassName("close-3")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
